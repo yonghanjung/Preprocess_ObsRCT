046e123aa96d68d3e233e8ff166e517600224ede
Hans Jung <yhansjung@gmail.com>
2018-05-31 12:23:21 -0400

    180531 merged

--- ARDS_firstday.py
+++ ARDS_firstday.py
@@ -23,7 +23,7 @@ class ARDS_firstday():
             dict_bloodgas[key_elem] = []
 
         for idx in range(len(self.ARDS_ID)):
-            print(idx/len(self.ARDS_ID))
+            print(idx/len(self.ARDS_ID), idx)
             subject_id = int(self.ARDS_ID.iloc[idx]['SUBJECT_ID'])
             icustay_id = int(self.ARDS_ID.iloc[idx]['ICUSTAY_ID'])
             hadm_id = int(self.ARDS_ID.iloc[idx]['HADM_ID'])
@@ -35,6 +35,7 @@ class ARDS_firstday():
             subject_bloodgas_mask = (self.bloodgas['subject_id'] == subject_id) & \
                             (self.bloodgas['icustay_id'] == icustay_id) & \
                             (self.bloodgas['hadm_id'] == hadm_id)
+
             subject_bloodgas = self.bloodgas[subject_bloodgas_mask][list_keys]
             for key_elem in list_keys:
                 list_elem = list(subject_bloodgas[pd.notnull(subject_bloodgas[key_elem])][key_elem])
@@ -45,10 +46,33 @@ class ARDS_firstday():
 
     def firstday_merge(self):
         bloodgas = self.bloodgas_average()
+
+        print("bloodgas", len(bloodgas))
+        print("gcs", len(self.gcs))
+        print("uo", len(self.uo))
+        print("vitals", len(self.vitals))
+        print("labs_crea", len(self.labs_crea))
+
+
         merged = pd.merge(bloodgas, self.gcs, on=['subject_id', 'hadm_id', 'icustay_id'])
         merged = pd.merge(merged, self.uo, on=['subject_id', 'hadm_id', 'icustay_id'])
         merged = pd.merge(merged, self.vitals, on=['subject_id', 'hadm_id', 'icustay_id'])
         merged = pd.merge(merged, self.labs_crea, on=['subject_id', 'hadm_id', 'icustay_id'])
+
+        merged.columns = ['calcium', 'carboxyhemoglobin', 'chloride', 'glucose', 'HADM_ID',
+                          'hematocrit', 'hemoglobin', 'ICUSTAY_ID', 'intubated', 'lactate',
+                          'methemoglobin', 'o2flow', 'pao2fio2', 'pco2', 'peep', 'ph', 'po2',
+                          'potassium', 'requiredo2', 'so2', 'sodium', 'spo2', 'SUBJECT_ID',
+                          'temperature', 'tidalvolume', 'totalco2', 'ventilationrate',
+                          'ventilator', 'mingcs', 'gcsmotor', 'gcsverbal', 'gcseyes',
+                          'endotrachflag', 'urineoutput', 'heartrate_min', 'heartrate_max',
+                          'heartrate_mean', 'sysbp_min', 'sysbp_max', 'sysbp_mean', 'diasbp_min',
+                          'diasbp_max', 'diasbp_mean', 'meanbp_min', 'meanbp_max', 'meanbp_mean',
+                          'resprate_min', 'resprate_max', 'resprate_mean', 'tempc_min',
+                          'tempc_max', 'tempc_mean', 'spo2_min', 'spo2_max', 'spo2_mean',
+                          'glucose_min', 'glucose_max', 'glucose_mean', 'creatinine_min',
+                          'creatinine_max']
+
         return merged
 
 if __name__ == '__main__':
--- main.py
+++ main.py
@@ -8,6 +8,7 @@ ARDS_ID = pickle.load(open('PKL/ARDS_ID.pkl','rb'))
 # Load Demographic information
 ARDS_Age = pickle.load(open('PKL/ARDS_Age.pkl','rb'))
 ARDS_HeightWeight = pickle.load(open('PKL/ARDS_HeightWeight.pkl','rb'))
+
 ARDS_Gender = pickle.load(open('PKL/ARDS_Gender.pkl','rb'))
 ARDS_DOD = pickle.load(open('PKL/ARDS_DOD.pkl','rb'))
 
@@ -22,3 +23,29 @@ ARDS_VITALLAB = pickle.load(open('PKL/ARDS_firstday.pkl','rb'))
 
 # Load Diagnoses information
 ARDS_DIAGNOSES = pickle.load(open('PKL/ARDS_diagnoses.pkl','rb'))
+
+# Matching with IDs
+merged = pd.merge(ARDS_ID, ARDS_Age, on=['SUBJECT_ID'])
+merged = pd.merge(merged, ARDS_HeightWeight, on=['SUBJECT_ID', 'ICUSTAY_ID'])
+merged = pd.merge(merged, ARDS_Gender, on=['SUBJECT_ID'])
+merged = pd.merge(merged, ARDS_DOD, on=['SUBJECT_ID'])
+merged = pd.merge(merged, ARDS_VENT, on=['SUBJECT_ID','HADM_ID','ICUSTAY_ID'])
+merged = pd.merge(merged, ARDS_DRUG, on=['SUBJECT_ID','HADM_ID','ICUSTAY_ID'])
+merged = pd.merge(merged, ARDS_DIAGNOSES, on=['SUBJECT_ID','HADM_ID'])
+merged = pd.merge(merged, ARDS_VITALLAB, on=['SUBJECT_ID','HADM_ID'])
+
+# CSV Export
+merged.to_csv('df_to_csv/merged.csv',sep="|",na_rep="-")
+
+# ARDS_ID.to_csv('df_to_csv/ARDS_ID.csv',sep="|",na_rep="-")
+# ARDS_Age.to_csv('df_to_csv/ARDS_Age.csv',sep="|",na_rep="-")
+# ARDS_HeightWeight.to_csv('df_to_csv/ARDS_HeightWeight.csv',sep="|",na_rep="-")
+# ARDS_Gender.to_csv('df_to_csv/ARDS_Gender.csv',sep="|",na_rep="-")
+# ARDS_DOD.to_csv('df_to_csv/ARDS_DOD.csv',sep="|",na_rep="-")
+# ARDS_VENT.to_csv('df_to_csv/ARDS_VENT.csv',sep="|",na_rep="-")
+# ARDS_DRUG.to_csv('df_to_csv/ARDS_DRUG.csv',sep="|",na_rep="-")
+# ARDS_VITALLAB.to_csv('df_to_csv/ARDS_VITALLAB.csv',sep="|",na_rep="-")
+# ARDS_DIAGNOSES.to_csv('df_to_csv/ARDS_DIAGNOSES.csv',sep="|",na_rep="-")
+
+
+
